12478b16a87871a46b0ca0d73c63d7ceb7889d67
aticie <efehanatici@gmail.com>
2024-07-18 23:29:37 +0200

    Integrate Sentry

--- ronnia/main.py
+++ ronnia/main.py
@@ -1,6 +1,10 @@
 import asyncio
 import multiprocessing
+import os
 import platform
+
+import sentry_sdk
+from sentry_sdk.integrations.logging import LoggingIntegration
 
 from ronnia.bots.bot_manager import BotManager
 from ronnia.utils.logger import RonniaLogger
@@ -8,6 +12,16 @@ from ronnia.utils.logger import RonniaLogger
 logger = RonniaLogger(__name__)
 
 if __name__ == "__main__":
+
+    sentry_sdk.init(
+        dsn=os.getenv("SENTRY_DSN"),
+        traces_sample_rate=1.0,
+        profiles_sample_rate=1.0,
+        integrations=[
+            LoggingIntegration(),
+        ],
+    )
+
     # Required for multiprocessing to work on Linux
     multiprocessing.set_start_method("spawn")
 
