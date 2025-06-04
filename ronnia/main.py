cbb2d0d115c9853c8fd26fd98400f246f602e868
aticie <efehanatici@gmail.com>
2025-06-04 11:32:01 +0200

    Fixed imports and default environment variables

--- ronnia/main.py
+++ ronnia/main.py
@@ -5,29 +5,29 @@ import os
 import platform
 import sys
 
-import sentry_sdk
-from sentry_sdk.integrations.logging import LoggingIntegration
-
 from ronnia.bots.bot_manager import BotManager
 from utils.logger import CustomJsonFormatter
 
 if __name__ == "__main__":
 
-    sentry_sdk.init(
-        dsn=os.getenv("SENTRY_DSN"),
-        traces_sample_rate=1.0,
-        profiles_sample_rate=1.0,
-        integrations=[
-            LoggingIntegration(),
-        ],
-    )
+    if os.getenv("SENTRY_DSN"):
+        import sentry_sdk
+        from sentry_sdk.integrations.logging import LoggingIntegration
+        sentry_sdk.init(
+            dsn=os.getenv("SENTRY_DSN"),
+            traces_sample_rate=1.0,
+            profiles_sample_rate=1.0,
+            integrations=[
+                LoggingIntegration(),
+            ],
+        )
 
     # Required for multiprocessing to work on Linux
     multiprocessing.set_start_method("spawn")
 
     logger = logging.getLogger()
 
-    logger.setLevel(os.getenv("LOG_LEVEL"))
+    logger.setLevel(os.getenv("LOG_LEVEL", logging.INFO))
     logHandler = logging.StreamHandler(sys.stdout)
     formatter = CustomJsonFormatter('%(timestamp)s %(level)s %(name)s %(message)s')
     logHandler.setFormatter(formatter)
